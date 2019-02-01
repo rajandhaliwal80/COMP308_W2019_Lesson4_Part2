fe2822459ef7ed10b336c6451504e2176608a9f9
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-01-31 20:29:32 -0500

    updated package.json

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,14 @@
-/* cutom Javascript goes here*/
+/* custom Javascript goes here*/
+
+//IFE -- Immediately Invoked Function Expression
+
+(function(){
+
+function Start(){
+console.log(`%c App Started...`,
+"font-size: 20px; color: blue; font-weight: bold");
+}
+
+window.addEventListener("load", Start);
+
+})();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
 module.exports = router;
